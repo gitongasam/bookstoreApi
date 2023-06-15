759d5025de72e05a50b212eaeb12faab64d797da
Developer sam <gitongas684@gmail.com>
2023-06-15 12:35:07 +0300

    merged

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,31 +1,34 @@
 const mssql = require('mssql');
 const config = require('../config/config');
 
-async function getAllBooks(req, res) {
+async function borrowBook(req, res) {
   try {
-    let sql = await mssql.connect(config);
-    if (sql.connected) {
-      let results = await sql.query('SELECT * FROM dbo.Books');
-      res.json(results.recordset);
-    }
-  } catch (error) {
-    console.error('Error retrieving books:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
+    const bookID = req.params.id;
 
-async function getAllMembers(req, res) {
-  try {
-    let sql = await mssql.connect(config);
-    if (sql.connected) {
-      let results = await sql.query('SELECT * FROM dbo.Members');
-      res.json(results.recordset);
-    }
-  } catch (error) {
-    console.error('Error retrieving members:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-}
+    const sql = await mssql.connect(config);
+
+
+
+// async function registerMember(req, res) {
+//   try {
+//     const {MemberID, Name, Address, ContactNumber } = req.body;
+
+//     // Validate the input data (e.g., check for required fields, format validation, etc.)
+
+
+//     if (sql.connected) {
+//       // Insert the new member into the Members table
+//       const result = await sql.query(
+//         `INSERT INTO dbo.Members (MemberID, Name, Address, ContactNumber) VALUES ('${MemberID}', '${Name}', '${Address}', '${ContactNumber}')`
+//       );
+
+//       res.status(201).json({ message: 'Member registered successfully', memberID });
+//     }
+//   } catch (error) {
+//     console.error('Error registering member:', error);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// }
 
 async function registerMember(req, res) {
   try {
@@ -40,46 +43,35 @@ async function registerMember(req, res) {
       let result = await sql.query(query);
       console.log(result);
 
-      // Send the inserted book data as the response
-      res.json({ MemberId, Name, Address, ContactNumber });
-    }
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error('Error borrowing book:', error);
+    res.status(500).send('An error occurred while borrowing the book.');
   }
 }
 
-async function getMemberById(req, res) {
+async function returnBook(req, res) {
   try {
-    const memberId = req.params.id;
+    const bookID = req.params.id;
 
-    // Connect to the SQL Server
     const sql = await mssql.connect(config);
-    if (sql.connected) {
-      // Query the Members table for the specific member
-      const result = await sql.query(
-        `SELECT * FROM dbo.Members WHERE MemberID = ${memberId}`
-      );
 
-      if (result.recordset.length === 0) {
-        res.status(404).json({ message: 'Member not found' });
-      } else {
-        const member = result.recordset[0];
-        res.status(200).json(member);
-      }
-    } else {
-      res.status(500).json({ error: 'Database connection error' });
-    }
+    const result = await sql
+      .request()
+      .input('bookID', mssql.Int, bookID)
+      .query(`UPDATE dbo.Books
+              SET Status = 'Available'
+              WHERE BookID = @bookID;
+              SELECT * FROM dbo.Books WHERE BookID = @bookID`);
+    const returnedBook = result.recordset[0];
+    res.status(200).send(`Book returned successfully. \n\nBook details: ${JSON.stringify(returnedBook)}`);
+
   } catch (error) {
-    console.error('Error retrieving member:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('Error returning book:', error);
+    res.status(500).send('An error occurred while returning the book.');
   }
 }
-
 
 module.exports = {
-  getAllBooks,
-  getAllMembers,
-  registerMember,
-  getMemberById
+  borrowBook,
+  returnBook,
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const { getAllBooks, getAllMembers, registerMember, getMemberById } = require('../controllers/controllers');
 
 const router = express.Router();
@@ -7,6 +8,9 @@ router.get('/', getAllBooks)
 router.get('/allmembers',getAllMembers)
 router.post('/members',registerMember)
 router.get('/memberbyid/:id',getMemberById)
+router.get('/books/borrow/:id', borrowBook)
+router.get('/books/return/:id', returnBook)
+
 
 
 module.exports = router;
