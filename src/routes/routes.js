dc24525b6b08bf660065c0ee0c743f5a812d5542
Developer sam <gitongas684@gmail.com>
2023-06-15 12:19:50 +0300

    add members

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { getAllBooks } = require('../controllers/controllers');
+const { getAllBooks, getAllMembers, registerMember, getMemberById } = require('../controllers/controllers');
 
 const router = express.Router();
 
 router.get('/', getAllBooks)
+router.get('/allmembers',getAllMembers)
+router.post('/members',registerMember)
+router.get('/memberbyid/:id',getMemberById)
+
 
 module.exports = router;
